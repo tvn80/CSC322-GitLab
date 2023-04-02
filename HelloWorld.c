92ed5d30ff67a347d1b87280e46882e866f2306a
Tony  Ngo <tonyngo@Tonys-MacBook-Air-3.local>
2023-04-02 15:45:42 -0400

    Commit #5

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,7 +3,7 @@
 //-----------------------------------------------------------------------------
 int main(void) {
 
-    printf("Hello Very Crool World!\n");
+    printf("Hello Very Happy World!\n");
 
     return(EXIT_SUCCESS);
 }
